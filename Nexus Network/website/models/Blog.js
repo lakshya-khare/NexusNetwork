dd0e827e8ec55c8c2dbeccee86dac2165c403852
Lakshya Khare <148869475+lakshya-khare@users.noreply.github.com>
2024-10-16 15:57:55 +0530

    del

--- Nexus Network/website/models/Blog.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-const BlogSchema = new mongoose.Schema({
-  title: String,
-  content: String,
-  createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User' } // Reference to the blogger who created
-});
-module.exports = mongoose.model('Blog', BlogSchema);
--- Nexus Network/website/models/Material.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const MaterialSchema = new mongoose.Schema({
-  title: String,
-  link: String,
-  type: String, // "notes", "video", "pdf", etc.
-  uploadedBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User' } // Reference to the mentor who uploaded
-});
-module.exports = mongoose.model('Material', MaterialSchema);
